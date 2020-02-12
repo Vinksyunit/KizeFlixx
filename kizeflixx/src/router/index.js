878cbac758a95d9177dcff195b65c3458a6cd745
aqodad <aqodad.ouahid26@gmail.com>
2020-02-12 21:57:56 +0100

    add navbar and searchBar in App.vue

--- kizeflixx/src/router/index.js
+++ kizeflixx/src/router/index.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Latest from '@/components/Latest'
+import Movie from '@/components/Movie'
+
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
     routes: [
         {
-            
-        }
+            path: '/',
+            name: 'Latest',
+            component: Latest
+        },
+        {
+            path: '/movie/:id',
+            name: 'Movie',
+            props: true,
+            component: Movie
+          },
     ]
 })
